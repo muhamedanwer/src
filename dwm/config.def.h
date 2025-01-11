0507c1e3fdf40d7b01c56d855decca16d157edf6
Mohamed Anwar <76420528+muhamed-anwer@users.noreply.github.com>
2025-01-11 11:24:12 +0200

    Add files via upload

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,34 +1,34 @@
 /* See LICENSE file for copyright and license details. */
 
-
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gap pixel between windows */
+static const unsigned int gappx     = 1;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Wuncon Siji:size=10", "xos4 Terminus:size=9"  };
-static const char dmenufont[] = "xos4 Terminus:size=9";
-static const char normbordercolor[] = "#212121";
-static const char selbordercolor[]  = "#212121";
-static const char normbgcolor[]     = "#1C1C1C";
-static const char normfgcolor[]     = "#C0C0C0";
-static const char selbgcolor[]      = "#111111";
-static const char selfgcolor[]      = "#FDFDFD";
+static const char *fonts[]          = { "Hack Nerd Font Mono:size=10" };
+static const char dmenufont[]       = "Hack Nerd Font Mono:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#696969";
 
-static const char col1[] = "#212121";
-static const char col2[] = "#1C1C1C";
-static const char col3[] = "#696969";
-static const char col0[] = "#C0C0C0";
 
 static const char *colors[][3]      = {
-	/*               fg    bg    border   */
-	[SchemeNorm] = { col3, col2, col1},
-	[SchemeSel]  = { col0, col1, col1},
-};
 
+/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+
+};
 /* tagging */
-static const char *tags[] = { "\uE01E prod", "\uE01E dev", "\uE01E term", "\uE0B2 web", "\uE1EB media", "\uE0BE misc" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -66,8 +66,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col3, "-nf", col1, "-sb", col2, "-sf", col3, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
